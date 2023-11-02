d6bfe283f178d58dbd1e8113ccc760f823a3578a
BaoPhongk21 <149062733+BaoPhongk21@users.noreply.github.com>
2023-11-02 13:14:02 +0700

    Add files via upload

---
+++ Operator/main.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+/* Tinh tong hieu tich thuong cua so a va b*/
+
+int main(int argc, char *argv[]) {
+	
+//	int a, b;
+//	int tong, hieu, tich;
+//	float thuong;
+//	printf("Nhap 2 so nguyen a,b vao:\n");
+//	scanf("%d%d", &a, &b);
+//	
+//	 tong = a + b;
+//	 hieu = a - b;
+//	 tich = a * b;
+//	 
+//	 printf("%d + %d = %d\n", a, b, tong);
+//	 printf("%d - %d = %d\n", a, b, hieu);
+//	 printf("%d * %d = %d\n", a, b,tich);
+//	 
+//	 if(b == 0)
+//	{
+//		printf("Khong the thuc hien phep chia\n");
+//		}else{
+//			thuong = (float)a/b;
+//			printf("%d / %d = %f \n", a, b, thuong);
+//			
+//		}
+
+
+	/* Khai bao bien c gan gia tri bien c cho phep taon so sanh cua a va b.
+	Thuc thi cac toan tu so sanh >,<,=>,>=.Moi bieu thuc gan xong in ket qua ra man hinh
+	*/
+	
+
+// 	 int a, b;
+//	  printf("Nhap a vao:");
+// 	 scanf("%d", &a);
+// 	 printf("Nhap b vao:");
+//  	scanf("%d", &b);
+// 	 
+//  	printf("%d > %d la %d  \n", a, b, a > b);
+//	printf("%d == %d la %d \n", a, b, a == b);
+//    printf("%d >= %d la %d  \n", a, b, a >= b);
+//    printf("%d <= %d la %d \n", a, b, a <= b);
+
+
+	return 0;
+}
